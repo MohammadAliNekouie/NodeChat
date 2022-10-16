c5fb315f849a4c5af1d8a48c83641a50dad53660
Mohammad Ali Nekouie <mohammadalinekouie@gmail.com>
2022-10-16 17:32:06 +0330

    base of Live chat with NodeJS by Mohammad Ali Nekouie

---
+++ main.js
@@ -0,0 +1,37 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/javascript.js to edit this template
+ */
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+         socket.on('connect', () => {
+            console.log('New websocket Connected');
+        });
+         socket.on('disconnect', () => {
+            console.log('a websocket disconnected');
+        });
+        socket.on('chat message', function(msg){
+            console.log(msg)
+            io.emit('chat message', msg);
+        });
+        socket.on('image', function(msg){
+          socket.emit('image', msg);
+        });
+        socket.on('error', function (err) {
+         if (err.description) throw err.description;
+         else throw err; // Or whatever you want to do
+       });  
+});
+
+http.listen(port, function(){
+  console.log('listening on *:' + port);
+});
+
---
+++ readme.md
@@ -0,0 +1,8 @@
+This is an Opensource Online Live Chat 
+done by NodeJs thanks to Socket.IO
+
+How to run it:
+1 - upload all files in a directory on your host like /chat
+2 - Install NodeJs on your machine and run main.js on it (via PM2)
+3 - open the browser on a client and go to your host/chat directory
+4 - enjoy the chat !
